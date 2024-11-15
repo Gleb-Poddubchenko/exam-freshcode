c4d77fbc6dcc5a85a5e09bc72ab45cc1fbcc8a79
Glebasta <hlibpoddubchenko@gmail.com>
2024-11-15 11:24:07 +0100

    Add components HowItWorks

---
+++ client/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './HeroSection.module.scss';
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <h1>Как это работает</h1>
+      <p>Краткое описание страницы, которое объясняет, как работает сервис.</p>
+    </div>
+  );
+};
+
+export default HeroSection;
