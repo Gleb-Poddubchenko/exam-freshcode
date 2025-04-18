8b02e2d2e2153783c6f1acc8d599edb7ef0c8040
Glebasta <hlibpoddubchenko@gmail.com>
2025-04-18 23:44:05 +0200

    Update checkToken

--- server/src/middlewares/checkToken.js
+++ server/src/middlewares/checkToken.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
 const CONSTANTS = require('../constants');
 const TokenError = require('../errors/TokenError');
-const userQueries =require('../controllers/queries/userQueries');
+const userQueries = require('../controllers/queries/userQueries');
 
 module.exports.checkAuth = async (req, res, next) => {
-  const accessToken = req.headers.authorization;
-  if (!accessToken) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
     return next(new TokenError('need token'));
   }
+
+  const token = authHeader.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : authHeader;
+
   try {
-    const tokenData = jwt.verify(accessToken, CONSTANTS.JWT_SECRET);
+    const tokenData = jwt.verify(token, CONSTANTS.JWT_SECRET);
     const foundUser = await userQueries.findUser({ id: tokenData.userId });
     res.send({
       firstName: foundUser.firstName,
@@ -22,19 +27,24 @@ module.exports.checkAuth = async (req, res, next) => {
       email: foundUser.email,
     });
   } catch (err) {
-    next(new TokenError());
+    next(new TokenError('invalid token'));
   }
 };
 
 module.exports.checkToken = async (req, res, next) => {
-  const accessToken = req.headers.authorization;
-  if (!accessToken) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
     return next(new TokenError('need token'));
   }
+
+  const token = authHeader.startsWith('Bearer ')
+    ? authHeader.split(' ')[1]
+    : authHeader;
+
   try {
-    req.tokenData = jwt.verify(accessToken, CONSTANTS.JWT_SECRET);
+    req.tokenData = jwt.verify(token, CONSTANTS.JWT_SECRET);
     next();
   } catch (err) {
-    next(new TokenError());
+    next(new TokenError('invalid token'));
   }
 };
